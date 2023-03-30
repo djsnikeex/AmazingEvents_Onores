2951bb120f068ce5f4eb431b05e8130c8b2502c1
djsnikeex <nicolasonores@hotmail.com>
2023-03-30 01:58:43 -0300

    VUEJS AmazingEvents

--- js/stats.js
+++ js/stats.js
@@ -1,22 +1,3 @@
-/* import { targetpast,targetFuture, drawEventsStats,drawTableForCategory} from '../js/functions.js';
-
-let Upcoming = document.getElementById('Upcoming');
-let PastEvents = document.getElementById('PastEvents');
-let Stats = document.getElementById('Stats');
-
-async function iniciar(){
-    fetch('../data/amazing.json')
-                .then(response => response.json())
-                .then(data => {
-                    drawEventsStats(Stats,data)
-                    drawTableForCategory(PastEvents,targetpast(data));
-                    drawTableForCategory(Upcoming,targetFuture(data));
-                })
-                .catch(error => console.log(error));
-            }
-await iniciar();
- */
-
 const { createApp } = Vue;
 const app = createApp({
     data() {
